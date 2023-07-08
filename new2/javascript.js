bf8683802de9a533b58ed4559bad80f4291bd36d
时过境迁Wayne <3462757126@qq.com>
2023-07-08 11:15:39 +0000

    new

--- new2/javascript.js
+++ new2/javascript.js
@@ -1,108 +1,3 @@
-//听写按钮触发
-/* function tingxie() {
-    document.getElementById("txtJSON").value = ""; //清除输入框内容
-    document.getElementById('display').style.display = 'none'; //隐藏输出单词
-    document.getElementById('good').style.display = 'block'; //显示提示
-    document.getElementById("xianshi-1").innerHTML = "<b>显示单词</b>"; //更改按钮名称
-    document.getElementById("xianshi-1").style.fontFamily = "Microsoft Yahei"; //调整按钮字体
-    document.getElementById("xianshi-1").style.fontSize = "25px"; //设置显目的字体
-    document.getElementById("xianshi-1").removeAttribute("disabled"); //去掉不可点击			
-    document.getElementById("xianshi-1").style.backgroundColor = 'rgba(255, 255, 255, 0.5)'; //设置背景色		 */
-
-/* }
- */
-//显示单词按钮触发
-/* function xianshi() {
-    document.getElementById("xianshi-1").setAttribute("disabled", true); //设置不可点击
-    document.getElementById('display').style.display = 'block'; //显示输出单词
-    document.getElementById('good').style.display = 'none'; //隐藏“隐藏提示” */
-    /* 隐藏单词倒计时 */
-   /*  document.getElementById("xianshi-1").innerHTML = "隐藏单词(10)"; //10
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(9)"; //9
-    }, 1000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(8)"; //8
-    }, 2000);
-
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(7)"; //7
-    }, 3000);
-
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(6)"; //6
-    }, 4000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(5)"; //5
-    }, 5000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(4)"; //4
-    }, 6000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(3)"; //3
-    }, 7000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(2)"; //2
-    }, 8000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "隐藏单词(1)"; //1
-    }, 9000); */
-    /* 执行隐藏单词倒计时结束 */
-
-    /* 执行隐藏单词 */
-   /*  setTimeout(function () {
-        document.getElementById('display').style.display = 'none'; //隐藏输出单词
-        document.getElementById('good').style.display = 'block'; //显示提示框
-        document.getElementById("good").innerHTML = "显示单词时间（10秒）结束，如需再次显示，请点击“显示单词”"; //更改隐藏提示内容 */
-        /* 设置按钮不可点击颜色 */
-        /* document.getElementById("xianshi-1").style.backgroundColor = '#555555'; //设置背景色
-        document.getElementById("xianshi-1").innerHTML = "请先思考！(5)"; //更改按钮名称
-    }, 10000); */
-    /* 执行隐藏单词结束 */
-
-    /* 开始倒计时 */
-    /* setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "请先思考！(4)"; //4
-    }, 11000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "请先思考！(3)"; //3
-    }, 12000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "请先思考！(2)"; //2
-    }, 13000);
-    setTimeout(function () {
-        document.getElementById("xianshi-1").innerHTML = "请先思考！(1)"; //1
-    }, 14000); */
-    /* 倒计时结束 */
-   /*  setTimeout(function () { */
-        /* 设置按钮可以点击（5秒后） */
-        /* document.getElementById("xianshi-1").removeAttribute("disabled"); //去掉不可点击			
-        document.getElementById("xianshi-1").style.backgroundColor = 'rgba(255, 255, 255, 0.5)'; //设置背景色	
-        document.getElementById("xianshi-1").innerHTML = "显示单词"; //更改回按钮名称
-    }, 15000);
-
-} */
-
-
-// 底部信息最底部
-/* $(function () {
-    function footerPosition() {
-        $("footer").removeClass("fixed-bottom");
-        //网页正文全文高度
-        var contentHeight = document.body.scrollHeight,
-            //可视窗口高度，不包括浏览器顶部工具栏
-            winHeight = window.innerHeight;
-        if (!(contentHeight > winHeight)) {
-            //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
-            $("footer").addClass("fixed-bottom");
-        } else {
-            $("footer").removeClass("fixed-bottom");
-        }
-    }
-    footerPosition();
-    $(window).resize(footerPosition);
-}); */
-
 //其他
 function pronance(e) {
     var pn = document.getElementById("inputGroupSelect01");
@@ -113,10 +8,10 @@ function pronance(e) {
         }
     }
     var audio = $("#audio");
-    audio.attr("src", "http://dict.youdao.com/dictvoice?type=0&audio=" + $(e).attr("class") /* + "&type=" + p */);
+    audio.attr("src", "https://dict.youdao.com/dictvoice?type=0&audio=" + $(e).attr("class") /* + "&type=" + p */);
     audio.get(0).play();
 
-    $("#dictHcContent").attr("src", "http://dict.cn/apis/dict3.php?q=" + $(e).attr("class"));
+    $("#dictHcContent").attr("src", "https://dict.cn/apis/dict3.php?q=" + $(e).attr("class"));
 
 }
 
@@ -133,13 +28,6 @@ function zhuanhuan() {
             // card bg-secondary text-white lead p-2 col
             /// style='float:left;width:50%;'
         };
-       /*  document.getElementById("xianshi-1").innerHTML = "显示单词(听写开始后使用)"; //更改名称
-        document.getElementById('xianshi-1').style.display = 'block'; //调整为显示按钮
-        document.getElementById("xianshi-1").setAttribute("disabled", true); //设置不可点击
-        document.getElementById("xianshi-1").style.backgroundColor = '#555555'; //设置背景色
-        document.getElementById("xianshi-1").style.fontSize = "15px"; //设置不显目的字体
-        document.getElementById("good").style.display = "none";
-        document.getElementById("display").style.display = "block" */
     }
 }
 
@@ -160,7 +48,7 @@ $("#dictation").click(function () {
     function myloop() {
         setTimeout(function () {
             var audio = $("#audio");
-            audio.attr("src", "http://dict.youdao.com/dictvoice?type=0&audio=" + $(childs[i]).attr("class")/*  + "&type=" + p */);
+            audio.attr("src", "https://dict.youdao.com/dictvoice?type=0&audio=" + $(childs[i]).attr("class")/*  + "&type=" + p */);
             audio.get(0).play();
             i++;
             if (i < childs.length) {
@@ -173,25 +61,6 @@ $("#dictation").click(function () {
 
 
 });
-
-
-//跳转判断
-/* if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i))) { //跳到手机端
-    window.location = "../wap.html";
-} */
-
-//“如何使用”
-function MsgBox() //声明标识符
-{
-    alert(
-        "使用说明： \n1、将单词按照一行一词粘贴到下面的输入框。 \n2、点击“转换”，然后下方会出现转换后的单词； \n\n·单击每个单词，会发出读音，右下角会出现释义。 \n\n·设定间隔时间，点击“听写”，即可按时间间隔，依次读出每个单词"); //弹出对话框
-}
-
-//“关于工具”
-function MsgBox2() //声明标识符
-{
-    alert("本工具由时过境迁Wayne 搭建运维，远古级源码来源于网络，本人大约用了累计8个小时维护、修复 \n\n反馈问题到：wayne0926@foxmail.com \n您的支持就是我的动力 \n\n版本：5.33           最近维护时间：2020/6/6/22:00"); //弹出对话框
-}
 
 //离开提示
 function myfunction() {
